1be86df573c8746b76affa33fd8b1c6755240862
phongdq <phongdq25@gmail.com>
2026-02-28 16:38:42 +0000

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-bash scripts/qwen/sft/maven/sft_qwen3_4B.sh
-bash scripts/qwen/sft/maven/sft_qwen2.5_0.5B.sh
-bash scripts/qwen/sft/maven/sft_qwen3_4B.sh
-bash scripts/qwen/sft/maven/sft_qwen3_4B.sh
+bash scripts/qwen/sft/ace/sft_qwen3_0.6B.sh
+bash scripts/qwen/distillm/train_0.6B_4B_v2_geneva.sh
+bash scripts/qwen/distillm/train_0.6B_4B_v2_ace_mix_geneva.sh
---
+++ scripts/qwen/sft/ace/sft_qwen3_0.6B.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+
+GPUS=(0 1)
+export CUDA_VISIBLE_DEVICES=$(IFS=,; echo "${GPUS[*]}")
+
+MASTER_ADDR=localhost
+MASTER_PORT=66$(($RANDOM%90+10))
+NNODES=1
+NODE_RANK=0
+GPUS_PER_NODE=${#GPUS[@]}
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE \
+                  --nnodes $NNODES \
+                  --node_rank $NODE_RANK \
+                  --master_addr $MASTER_ADDR \
+                  --master_port $MASTER_PORT"
+
+# model
+BASE_PATH=.
+CKPT_NAME="qwen3-0.6B"
+CKPT="results/qwen3/sft_0.6B_geneva/e5-bs8-lr5e-05-G2-N2-NN1/305"
+# data
+DATA_DIR="${BASE_PATH}/processed_data/ace_teacher/qwen/"
+# DATA_DIR="${BASE_PATH}/processed_data/qwen/"
+# hp
+BATCH_SIZE=8
+LR=0.00005
+GRAD_ACC=2
+EVAL_BATCH_SIZE=32
+EPOCHS=5
+# length
+MAX_LENGTH=768
+# runtime
+SAVE_PATH="${BASE_PATH}/results/qwen3/sft_0.6B_ace_cl"
+# seed
+SEED=42
+
+
+OPTS=""
+# model
+OPTS+=" --base-path ${BASE_PATH}"
+OPTS+=" --model-path ${CKPT}"
+OPTS+=" --ckpt-name ${CKPT_NAME}"
+OPTS+=" --model-type qwen"
+OPTS+=" --n-gpu ${GPUS_PER_NODE}"
+# OPTS+=" --gradient-checkpointing"
+# data
+OPTS+=" --data-dir ${DATA_DIR}"
+OPTS+=" --num-workers 0"
+OPTS+=" --dev-num -1"
+# hp
+OPTS+=" --lr ${LR}"
+OPTS+=" --batch-size ${BATCH_SIZE}"
+OPTS+=" --eval-batch-size ${EVAL_BATCH_SIZE}"
+OPTS+=" --gradient-accumulation-steps ${GRAD_ACC}"
+OPTS+=" --warmup-iters 0"
+OPTS+=" --warmup-ratio 0.1"
+OPTS+=" --lr-decay-style wrmup_cosine"
+# OPTS+=" --lr-decay-style cosine"
+OPTS+=" --weight-decay 1e-2"
+OPTS+=" --clip-grad 1.0"
+OPTS+=" --epochs ${EPOCHS}"
+# length
+OPTS+=" --max-length ${MAX_LENGTH}"
+OPTS+=" --max-prompt-length 460"
+# runtime
+OPTS+=" --do-train"
+OPTS+=" --do-valid"
+OPTS+=" --eval-gen"
+OPTS+=" --save-interval -1"
+OPTS+=" --eval-interval -1"
+OPTS+=" --log-interval 20"
+OPTS+=" --mid-log-num -1"
+OPTS+=" --save ${SAVE_PATH}"
+# seed
+OPTS+=" --seed ${SEED}"
+# deepspeed
+OPTS+=" --deepspeed"
+OPTS+=" --deepspeed_config ${BASE_PATH}/configs/deepspeed/ds_config_bf16.json"
+# type
+OPTS+=" --type lm"
+# gen
+OPTS+=" --do-sample"
+OPTS+=" --top-k 0"
+OPTS+=" --top-p 0.95"
+OPTS+=" --temperature 0.5"
+
+
+export NCCL_DEBUG=""
+export WANDB_DISABLED=True
+export TF_CPP_MIN_LOG_LEVEL=3
+export PYTHONPATH=${BASE_PATH}
+CMD="torchrun ${DISTRIBUTED_ARGS} ${BASE_PATH}/finetune_v2.py ${OPTS} $@"
+
+echo ${CMD}
+echo "PYTHONPATH=${PYTHONPATH}"
+mkdir -p ${SAVE_PATH}
+${CMD}
