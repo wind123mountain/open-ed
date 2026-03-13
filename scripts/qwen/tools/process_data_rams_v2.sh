75e42b0371982e61f9eda17ddfa64c72277d0f61
phongdq <phongdq25@gmail.com>
2026-03-13 07:21:43 +0000

    update

---
+++ scripts/qwen/tools/process_data_rams_v2.sh
@@ -0,0 +1,16 @@
+
+export TF_CPP_MIN_LOG_LEVEL=3
+
+
+for i in {0..4}
+do
+    PYTHONPATH=. python3 ./tools/process_data.py \
+        --data-dir ./data/rams_v2/${i}/ \
+        --processed-data-dir ./processed_data/rams_v2/${i} \
+        --model-path Qwen/Qwen3-0.6B \
+        --data-process-workers 4 \
+        --max-prompt-length 460 \
+        --t-max-prompt-length 640 \
+        --dev-num 1000 \
+        --model-type qwen
+done
