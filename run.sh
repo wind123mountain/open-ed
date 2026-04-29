45aa51ccecc439b184cc42ac679c3ae91298e767
phongdq25@gmail.com <phongdq>
2026-04-29 10:12:02 +0000

    update for 8gpu

--- run.sh
+++ run.sh
@@ -11,3 +11,8 @@ bash scripts/qwen/span_distillm/ace/train_1B_3B_llama.sh
 bash scripts/qwen/tools/process_data_ace_gemma.sh
 bash scripts/qwen/sft/sft_gemma_4B_ace.sh
 bash scripts/qwen/span_distillm/ace/train_1B_4B_gemma.sh
+bash scripts/qwen/ablation/ace/kd_ratio/train_0.6B_4B_0.1.sh
+bash scripts/qwen/ablation/ace/kd_ratio/train_0.6B_4B_0.3.sh
+bash scripts/qwen/ablation/ace/kd_ratio/train_0.6B_4B_0.5.sh
+bash scripts/qwen/ablation/ace/kd_ratio/train_0.6B_4B_0.7.sh
+bash scripts/qwen/ablation/ace/kd_ratio/train_0.6B_4B_1.0.sh
